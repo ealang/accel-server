dff329780c8e29adde81f1bcea8481a96410dff8
elang2 <ealang@gmail.com>
2019-09-01 14:22:44 -0400

    Add write to json feature

--- src/client/accel/accel_shell.py
+++ src/client/accel/accel_shell.py
@@ -1,5 +1,7 @@
 import json
 import argparse
+import os
+import sys
 from functools import partial
 from google.protobuf.json_format import MessageToDict
 
@@ -11,6 +13,32 @@ msg_to_dict = partial(
     preserving_proto_field_name=True,
     including_default_value_fields=True
 )
+
+
+def main_show_config(client):
+    print(json.dumps(msg_to_dict(client.config())))
+
+
+def main_stream(args, client):
+    history = []
+    try:
+        for batch in client.subscribe(args.batch_size):
+            for sample in batch:
+                json_sample = msg_to_dict(sample)
+                print(json.dumps(json_sample))
+                if args.dump is not None:
+                    history.append(json_sample)
+    except KeyboardInterrupt:
+        if args.dump is not None:
+            with open(args.dump, 'w') as fp:
+                json.dump(
+                    {
+                        'config': msg_to_dict(client.config()),
+                        'samples': history
+                    },
+                    fp,
+                    indent=2
+                )
 
 
 def main():
@@ -27,9 +55,14 @@ def main():
         help='Server port'
     )
     parser.add_argument(
-        '--dump-config',
+        '--show-config',
         action='store_true',
         help='Dump server config and exit'
+    )
+    parser.add_argument(
+        '--dump',
+        type=str,
+        help='Dump samples to given path'
     )
     parser.add_argument(
         '--batch-size',
@@ -39,17 +72,18 @@ def main():
     )
 
     args = parser.parse_args()
+    if args.dump is not None and os.path.exists(args.dump):
+        print(f'{args.dump} already exists')
+        sys.exit(1)
+
     client = AccelClient(
         addr=f'{args.host}:{args.port}'
     )
 
-    if args.dump_config:
-        print(json.dumps(msg_to_dict(client.config())))
+    if args.show_config:
+        main_show_config(client)
     else:
-        for batch in client.subscribe(args.batch_size):
-            print(json.dumps([
-                msg_to_dict(sample) for sample in batch
-            ]))
+        main_stream(args, client)
 
 
 if __name__ == '__main__':
